b2c4c1757c9c659ad18cf4f283553ca715b8fb02
Edier Gama <ediersantiago7@gmail.com>
2022-08-16 22:37:02 -0500

    Module Pattern, namespaces y métodos privados

--- Intermedio/roro.js
+++ Intermedio/roro.js
@@ -10,23 +10,50 @@ function createStudent({
     approvedCourses = [],
     learningPaths = [],
 } = {}) {
+    
 
-    return {
-       name,
-       age,
-       email,
-       learningPaths: [],
-       approvedCourses: [],
-       socialMedia: {
-        twitter,
-        instagram,
-        github,
-       }
+    const private = {
+        '_name': name,
     }
+    
+    const public = {
+     age,
+     email,
+     twitter,
+     instagram,
+     github,
+     approvedCourses,
+     learningPaths,
+
+     changeName(name){
+        private['_name'] = name
+     },
+     
+     readName(){
+        return private['_name']
+     },
+
+    }
+
+    Object.defineProperty(public, 'readName', {
+        writable: false,
+        configurable: false
+    })
+
+    Object.defineProperty(public, 'changeName', {
+        writable: false,
+        configurable: false
+    }) 
+    return public
+
 }
 
 const juan = createStudent({
     name: 'Juanito',
     age: 19,
-    email: 'juanito@frijolitos.com'
+    email: 'juanito@frijolitos.com',
+})
+
+Object.defineProperty(juan, 'name', {
+    writable: false
 })
